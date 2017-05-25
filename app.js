6cebd39a26a7524639ddefda92250950c0aff8ac
kernamx <powen.chang1@gmail.com>
2017-05-24 22:45:52 -0700

    localStorage

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ function displayImg() {
   var secondPath = document.createElement('img');
   var thirdPath = document.createElement('img');
 
-  firstPath.src = bag.path;
+  firstPath.src = chair.path;
   firstImg.appendChild(firstPath);
 
   secondPath.src = boots.path;
@@ -81,9 +81,9 @@ function randImg() {
   var randImgOne = getRandNum();
   first = imgArr[randImgOne];
 
-  //first.shown += 1;
-  //first.canShow = 2;
-  //timeOut.push(first);
+  first.shown += 1;
+  first.canShow = 2;
+  timeOut.push(first);
   imgArr.splice(randImgOne);
 
   var randImgTwo= getRandNum();
@@ -145,7 +145,8 @@ randImg();
 // updateTimeOut();
 
 
-
+//Still working on busMall first day assignment.
+//Once finish it I can apply that data to the chart.
 
 // var canvas = document.getElementById('chart');
 // var ctx = canvas.getContext('2d');
@@ -159,3 +160,45 @@ randImg();
 //   }
 //   options: {}
 // });
+
+//Still working on busMall first day assignment.
+var btn = document.getElementById('btn');
+var input = document.getElementById('item');
+var tasks = document.getElementById('tasks');
+
+if (localStorage.list) {
+  var list = localStorage.list.split(',');
+} else {
+  var list = [];
+}
+
+function save() {
+  list.push(input.value);
+  localStorage.list = list;
+}
+
+function create() {
+  var val = input.value;
+  var item = document.createElement('li');
+  item.appendChild(document.createTextNode(val));
+  tasks.appendChild(item);
+  input.value = '';
+}
+
+function load() {
+  if (localStorage.list) {
+    var item;
+    for (var x=0; x < list.length; x++) {
+      item = document.createElement('li');
+      item.appendChild(document.createTextNode(list[x]));
+      tasks.appendChild(item);
+    }
+  }
+}
+
+function render() {
+  save();
+  create();
+}
+
+load();
